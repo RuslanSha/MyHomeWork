f0a4dbaba599f182201758c54b593f78e859a0b3
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2023-12-26 14:06:56 +0300

    urgent resync

--- MyHomeWork/src/main/java/ru/homework/geometry/MyGeometry.java
+++ MyHomeWork/src/main/java/ru/homework/geometry/MyGeometry.java
@@ -4,7 +4,7 @@ import ru.homework.geometry.figures.Triangle;
 
 public class MyGeometry {
     public static void main(String[] args) {
-        var my_triangle = new Triangle(5.5, 6.7, 4.1);
+        var my_triangle = new Triangle(5.5, 6.7, 4.2);
 
         my_triangle.printProperties();
         System.out.println("Perimeter is " + my_triangle.perimeter());
--- MyHomeWork/src/test/java/ru/homework/geometry/figures/TriangleMethodTests.java
+++ MyHomeWork/src/test/java/ru/homework/geometry/figures/TriangleMethodTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TriangleMethodTests {
-
     @Test
     void canCalculatePerimetr() {
         var t = new Triangle(3.4, 8.1, 10.9);
         double result = t.perimeter();
-        Assertions.assertEquals(22.4, result);
+    //    Assertions.assertEquals(22.4, result);
+        if (!(result == 22.4)) {
+            throw new AssertionError(String.format("Expected %f, actual %f", 22.4, result));
+        }
     }
 
     @Test
@@ -17,5 +19,25 @@ public class TriangleMethodTests {
         var t = new Triangle(3.4, 8.1, 10.9);
         double result = t.area();
         Assertions.assertEquals(9.013589739942665, result);
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(3.4, -8.1, 10.9);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithIllegalSides() {
+        try {
+            new Triangle(3.4, 8.1, 1.9);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
     }
 }
